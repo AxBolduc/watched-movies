5dcf494050f963ec9697c45783c97eb36b6193ec
Alex Bolduc <aebolduc@wpi.edu>
2021-05-17 23:13:01 -0400

    Add modal to the trending page

--- client/src/pages/TrendingPage.jsx
+++ client/src/pages/TrendingPage.jsx
@@ -1,12 +1,14 @@
 import {Component} from 'react';
+import getMovieDetails from '../apis/tmdb';
 import {getTrendingMovies} from '../apis/trakt'
+import Modal from "../components/Modal/Modal";
 import Movie from "../components/Movie";
 
 class TrendingPage extends Component {
 
     constructor(props){
         super(props);
-        this.state = {movies: []}
+        this.state = {movies: [], modalMovie: {}, modalShow: false}
     }
 
     componentDidMount(){
@@ -17,15 +19,24 @@ class TrendingPage extends Component {
 
     render() { 
         return (
-            <>
-                <div className="movieContainer">
-                    {this.state.movies.map((movie) => (
-                        <Movie Key={movie.movie.ids.tmdb} {...movie.movie} />
-                    ))}
-                </div>
-
-            </>
-        );
+			<>
+				<div className="movieContainer">
+					{this.state.movies.map((movie) => (
+						<Movie Key={movie.movie.ids.tmdb} {...movie.movie} onClick={() =>{
+                            this.setState({modalShow: true})
+                            getMovieDetails(movie.movie.ids.tmdb).then((res)=>{
+                                this.setState({modalMovie: res.data});
+                            })
+                        }}/>
+					))}
+				</div>
+				<Modal
+					onClose={() => this.setState({ modalShow: false })}
+					movie={this.state.modalMovie}
+					show={this.state.modalShow}
+				/>
+			</>
+		);
     }
 }
  
