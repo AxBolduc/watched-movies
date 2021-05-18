9244869f507d180efb80a28b89d4547a0e418872
Alex Bolduc <aebolduc@wpi.edu>
2021-05-17 23:10:32 -0400

    Add modal to the watched movies page

--- client/src/pages/WatchedPage.jsx
+++ client/src/pages/WatchedPage.jsx
@@ -2,13 +2,19 @@ import {Component} from 'react';
 import Axios from 'axios';
 import {getWatchedMovies} from '../apis/trakt';
 import Movie from '../components/Movie';
+import Modal from "../components/Modal/Modal";
+import getMovieDetails from '../apis/tmdb';
 
 class WatchedPage extends Component {
 
     constructor(props){
         super(props)
-        this.state = {watchedMovies:[], isLogged: false}
-
+        this.state = {
+			watchedMovies: [],
+			isLogged: false,
+			modalMovie: {},
+			modalShow: false,
+		};
     }
 
     componentDidMount() {
@@ -27,22 +33,33 @@ class WatchedPage extends Component {
         }).catch((err) => {
             //do nothing
         });
-
-
-
     }
 
     render() { 
-        return ( 
-            <>
-                <div className="movieContainer">
-                    {this.state.watchedMovies.length > 0 && this.state.watchedMovies.map((movie) => (
-                        <Movie Key={movie.movie.ids.tmdb} {...movie.movie} />
-                    ))}
-                </div>
-
-            </>
-         );
+        return (
+			<>
+				<div className="movieContainer">
+					{this.state.watchedMovies.length > 0 &&
+						this.state.watchedMovies.map((movie) => (
+							<Movie
+								Key={movie.movie.ids.tmdb}
+								{...movie.movie}
+                                onClick={()=>{
+                                    this.setState({modalShow: true})
+                                    getMovieDetails(movie.movie.ids.tmdb).then((res)=>{
+                                        this.setState({modalMovie: res.data})
+                                    })
+                                }}
+							/>
+						))}
+				</div>
+				<Modal
+					onClose={() => this.setState({modalShow: false})}
+					movie={this.state.modalMovie}
+					show={this.state.modalShow}
+				/>
+			</>
+		);
     }
 }
  
