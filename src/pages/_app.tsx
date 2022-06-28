19c8174a8544755a64a28b3229729f7ffe3fbcaf
Alex Bolduc <aebolduc@wpi.edu>
2022-06-27 22:42:58 -0400

    Fix prod env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,8 +9,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
 export default withTRPC<AppRouter>({
   config({ctx}){
-    const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_URL
+    ? `https://${process.env.NEXT_PUBLIC_URL}/api/trpc`
     : 'http://localhost:3000/api/trpc';
     return {
       url
