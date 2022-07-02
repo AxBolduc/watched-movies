22f7fa778a4f874d34de8fa8b53a3b98b9aedca2
Alex Bolduc <aebolduc@wpi.edu>
2022-07-02 13:14:08 -0400

    Add nav bar animations

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,17 +7,17 @@ export const NavBar: React.FunctionComponent = () => {
         <nav className="px-4 bg-stone-800 flex justify-between items-center rounded-b-lg">
             <div className="flex justify-around gap-8 w-max">
                 <Link href="/">
-                    <div className="px-8 py-4 hover:bg-stone-600 text-white font-bold cursor-pointer">
+                    <div className="px-8 py-4 hover:bg-stone-600 text-white font-bold cursor-pointer transition-all duration-300">
                         Home
                     </div>
                 </Link>
                 <Link href="/trending">
-                    <div className="px-8 py-4 hover:bg-stone-600 text-white font-bold cursor-pointer">
+                    <div className="px-8 py-4 hover:bg-stone-600 text-white font-bold cursor-pointer transition-all duration-300">
                         Trending
                     </div>
                 </Link>
                 <Link href="/watched">
-                    <div className="px-8 py-4 hover:bg-stone-600 text-white font-bold cursor-pointer">
+                    <div className="px-8 py-4 hover:bg-stone-600 text-white font-bold cursor-pointer transition-all duration-300">
                         Watched
                     </div>
                 </Link>
